b1beedf0604fdc5b0366f3cf6c28207c93aa825f
Gustavo Ribeiro <gusribeiro@gmail.com>
2016-07-08 20:57:17 -0300

    ajustes no responsivo

--- blog/js/script.js
+++ blog/js/script.js
@@ -1,3 +1,17 @@
 $(function() {
 	$('.menu li').last().addClass('active');
+
+	$('span.responsive_ico_menu, .menu li').click(function() {
+		if ($('span.responsive_ico_menu').hasClass('active')) {
+			$('.menu').animate({
+				left: '-100%'
+			}).height('auto');
+		} else {
+			var h = $(window).innerHeight() - $('.header').outerHeight(true);
+			$('.menu').animate({
+				left: 0
+			}).height(h);
+		}
+		$('span.responsive_ico_menu').toggleClass('active');
+	});
 });
--- site/js/script.js
+++ site/js/script.js
@@ -135,16 +135,18 @@ $(function() {
 	});
 
 	$('span.responsive_ico_menu, .menu li').click(function() {
-		if ($('span.responsive_ico_menu').hasClass('active')) {
-			$('.menu').animate({
-				left: '-100%'
-			}).height('auto');
-		} else {
-			var h = $(window).innerHeight() - $('.header').outerHeight(true);
-			$('.menu').animate({
-				left: 0
-			}).height(h);
+		if ($('.responsive_ico_menu').is(':visible')) {
+			if ($('span.responsive_ico_menu').hasClass('active')) {
+				$('.menu').animate({
+					left: '-100%'
+				}).height('auto');
+			} else {
+				var h = $(window).innerHeight() - $('.header').outerHeight(true);
+				$('.menu').animate({
+					left: 0
+				}).height(h);
+			}
+			$('span.responsive_ico_menu').toggleClass('active');
 		}
-		$('span.responsive_ico_menu').toggleClass('active');
 	});
 });
